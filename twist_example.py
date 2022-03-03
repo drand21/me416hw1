6fc0eb7b6d1828788a55a55b576d98594ff34d66
drand21 <60789646+drand21@users.noreply.github.com>
2022-03-03 13:36:28 -0500

    Update twist_example.py

--- twist_example.py
+++ twist_example.py
@@ -1,8 +1,10 @@
-"""Example of how to set attributes in ROS messages"""
+""" Example of how to set attributes in ROS messages """
+
+from geometry_msgs.msg import Twist
 
 def twist_fill():
     """Creates new Twist object"""
-    #Populates the linear and angular coordinate values with 3.0
+    """Populates the linear and angular coordinate values with 3.0"""
     A = Twist()
     A.linear.x = 0.5
     A.linear.y = 0.5
