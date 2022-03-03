69c3d1d7f2957b4d7bcf51165de4d31b3b3fbb2b
drand21 <60789646+drand21@users.noreply.github.com>
2022-03-03 13:22:43 -0500

    Update motor_command_model.py

--- motor_command_model.py
+++ motor_command_model.py
@@ -1,7 +1,7 @@
 """Functions for modeling ROSBot"""
 
 import numpy as np
-from math import cos, sin
+#from math import cos, sin
 
 def model_parameters():
     """Returns two constant model parameters"""
@@ -9,19 +9,19 @@ def model_parameters():
     d = 0.5
     return k, d
 
-def system_matrix(theta):
-    """Returns a numpy array with the A(theta) matrix for a differential drive robot"""
-    return A
+#def system_matrix(theta):
+#    """Returns a numpy array with the A(theta) matrix for a differential drive robot"""
+#    return A
 
 
-def system_field(z, u):
-    """Computes the field at a given state for the dynamical model"""
-    return dot_z
+#def system_field(z, u):
+#    """Computes the field at a given state for the dynamical model"""
+#    return dot_z
 
 
-def euler_step(z, u, stepSize):
-    """Integrates the dynamical model for one time step using Euler's method"""
-    return zp
+#def euler_step(z, u, stepSize):
+#    """Integrates the dynamical model for one time step using Euler's method"""
+#    return zp
 
 
 def twist_to_speeds(speed_linear, speed_angular):
