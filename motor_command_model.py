1b40fa8afca68d07df9248a803e19a584694fef1
drand21 <60789646+drand21@users.noreply.github.com>
2022-03-03 13:25:19 -0500

    Update motor_command_model.py

--- motor_command_model.py
+++ motor_command_model.py
@@ -1,10 +1,10 @@
-"""Functions for modeling ROSBot"""
+#unctions for modeling ROSBot
 
 import numpy as np
 #from math import cos, sin
 
 def model_parameters():
-    """Returns two constant model parameters"""
+    #Returns two constant model parameters
     k = 1.0
     d = 0.5
     return k, d
@@ -25,14 +25,14 @@ def model_parameters():
 
 
 def twist_to_speeds(speed_linear, speed_angular):
-    """Takes the desired linear and angular velocity and returns normalized speeds for the left and right motor"""
-    """Inputs are in range of [-1.0, 1.0] and outputs are in same range"""
-    """Higher right value indicates robot will spin counter-clockwise, lower will spin clockwise, right=left indicates straight movement"""
+    #Takes the desired linear and angular velocity and returns normalized speeds for the left and right motor
+    #Inputs are in range of [-1.0, 1.0] and outputs are in same range
+    #Higher right value indicates robot will spin counter-clockwise, lower will spin clockwise, right=left indicates straight movement
     right = speed_linear*0.5
     left = speed_linear*0.5
-    """Linear and Angular velocities are given same weight"""
-    """An input linear speed of 1.0 will return a linear speed of 0.5 in both wheels"""
-    """An input angular speed of 1.0 on top of above speed will result in right wheel output at 1 while left wheel output at 0"""
+    #Linear and Angular velocities are given same weight
+    #An input linear speed of 1.0 will return a linear speed of 0.5 in both wheels
+    #An input angular speed of 1.0 on top of above speed will result in right wheel output at 1 while left wheel output at 0
     right = right + speed_angular*0.5
     left = left - speed_angular*0.5
     return left, right
